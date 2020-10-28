e170aa7ed2bb74817e212ce4d3c8cb84a462ba54
MorrisDefoe <artyom.mokretsov@gmail.com>
2020-10-29 01:56:54 +0200

    herroku commit

--- app/models/MigrationName.rb
+++ app/models/MigrationName.rb
@@ -1,4 +1,4 @@
-class MigrationName < ActiveRecord::Migration
+class MigrationName < ActiveRecord::Migration[6.0]
   def change
     add_foreign_key :users_orders, :users, column: :user_id, primary_key: "id"
     add_foreign_key :users_orders, :books, column: :book_id, primary_key: "id"
